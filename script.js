e1d1b466403a306874b54c2e0c0df00e4455d6ba
avilawebdesign <avilaweb@zohomail.eu>
2026-07-05 18:47:58 +0200

    Implement dynamic header style on scroll
    Add scroll event listener to change header style.

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
-console.log("Avila Web Design");
+window.addEventListener("scroll",function(){
+
+const header=document.querySelector("header");
+
+if(window.scrollY>80){
+
+header.style.background="rgba(5,18,35,.97)";
+header.style.boxShadow="0 10px 35px rgba(0,0,0,.25)";
+
+}else{
+
+header.style.background="rgba(7,27,51,.90)";
+header.style.boxShadow="none";
+
+}
+
+});
